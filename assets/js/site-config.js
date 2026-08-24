2213df0f2afe772957b92dad4d0fb526fd20330e
amyamyzhao <amyamyzhao0817@gmail.com>
2026-08-24 09:28:15 +0800

    Set UPILATE primary domain

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -6,5 +6,5 @@ window.UPILATE_CONFIG = {
   whatsappNumber: "8613175592862",
   web3formsAccessKey: "YOUR_WEB3FORMS_ACCESS_KEY",
   ga4MeasurementId: "YOUR_GA4_MEASUREMENT_ID",
-  siteUrl: "https://YOUR-DOMAIN.com"
+  siteUrl: "https://www.upilate.com"
 };
